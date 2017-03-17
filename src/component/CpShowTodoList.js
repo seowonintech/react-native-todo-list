3e8f662f7e89b9cc392ba585ef8917db14163959
Youngki Kang <yellowgg2@gmail.com>
2017-03-17 22:18:33 +0900

    add delete button

--- src/component/CpShowTodoList.js
+++ src/component/CpShowTodoList.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
 
 const styles = StyleSheet.create({
   textInput: {
-    height: 30, 
-    borderColor: 'gray', 
-    borderWidth: 1, 
-    alignItems: 'center', 
+    height: 30,
+    borderColor: 'gray',
+    borderWidth: 1,
+    alignItems: 'center',
     justifyContent: 'center'
   },
 });
@@ -18,24 +18,32 @@ class CpShowTodoList extends Component {
   }
 
   showList(todos, deleteTodo, onModifyTodoText) {
-  // {/*onPress={() => deleteTodo(todo.id)}*/}
-{/*onChangeText={(text) => onModifyTodoText(todo.id, text)}*/}    
+    // {/*onPress={() => deleteTodo(todo.id)}*/}
+    {/*onChangeText={(text) => onModifyTodoText(todo.id, text)}*/ }
     console.log('[KangLOG] todo.id at showList : ' + JSON.stringify(todos));
-    return todos.map((todo) => {      
+    return todos.map((todo) => {
       return (
-      <TextInput        
-        onChangeText={(text) => onModifyTodoText(todo.id, text)}
-        style={styles.textInput}
-        key={todo.id}
-        value={todo.text}
-      />
-    )});
+        <View key={todo.id + 2}>
+          <TextInput
+            onChangeText={(text) => onModifyTodoText(todo.id, text)}
+            style={styles.textInput}
+            key={todo.id}
+            value={todo.text}
+          />
+          <Button
+            key={todo.id + 1}
+            onPress={() => deleteTodo(todo.id)}
+            title='delete'
+          />
+        </View>
+      )
+    });
   }
 
   render() {
     const { todos, deleteTodo, onModifyTodoText } = this.props;
     return (
-      <View style={{flex: 1, backgroundColor: 'green'}}>
+      <View style={{ flex: 1, backgroundColor: 'green' }}>
         {this.showList(todos, deleteTodo, onModifyTodoText)}
       </View>
     );
