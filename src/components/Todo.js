741390c6fa2c16adcfab666bc8427fa15e112896
tshyeon <qvil1127@gmail.com>
2017-03-17 17:28:30 +0900

    [Uncomplete] Flex 연습

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import { TextInput, View } from 'react-native';
 
 const Todo = ({ todos, handleChangeText, mainState }) => (
-    <View>
+    // <View style={{flexDirection: 'column', width: 140, height: 100}}>
+    <View style={{flexDirection: 'column', flex: 0.95}}>
         {todos.map((todo, index) => (
             <TextInput
-                style={{width: mainState.deviceWidth, height: mainState.deviceHeight/20, borderColor: 'gray', borderWidth: 1}}
+                style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: 150,
+                    flexDirection: 'row', 
+                    borderColor: 'gray',
+                    borderWidth: 1
+                }}
                 key={index}
                 value={todo.text}
                 onChangeText={text => {
--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -8,13 +8,14 @@ import * as ActionTypes from '../actions/ActionTypes';
 import { addTodo, initTodo, setText } from '../actions';
 
 let AddTodo = ({ onClickAddButton, mainState, handleChangeText, onInitText }) => (
-    <View>
+    <View style={{height: 40, flexDirection:'row', flex: 0.05}}>
         <TextInput
-            style={{width: mainState.deviceWidth, height: mainState.deviceHeight/20, borderColor: 'gray', borderWidth: 1}}
+            style={{flex: 0.8, borderColor: 'gray', borderWidth: 1}}
             onChangeText={text => handleChangeText(text)}
             value={mainState.textInputValue}
         />
         <Button
+            style={{flex: 0.2}}
             title="Add"
             onPress={ () => {
                 if ( !mainState.textInputValue.trim() ) return;
