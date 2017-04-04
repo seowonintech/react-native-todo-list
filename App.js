2b9d53a8cb8888a4394552abbda6f4705115f40d
Youngki Kang <yellowgg2@gmail.com>
2017-04-04 13:20:45 +0900

    practicing firebase

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyBOz3euXj8nTuyHFW7FK6jbZXP_3FqwV8U",
+  authDomain: "react-todos-e69e3.firebaseapp.com",
+  databaseURL: "https://react-todos-e69e3.firebaseio.com",
+  projectId: "react-todos-e69e3",
+  storageBucket: "react-todos-e69e3.appspot.com",
+  messagingSenderId: "575260721031"
+};
+firebase.initializeApp(config);
+var databaseRef = firebase.database().ref();
 
 export default class App extends React.Component {
+
+  componentDidMount() {
+    databaseRef.on('value', function(snapshot) {
+      console.warn('[KangLOG] snapshwarnval() : ' + JSON.stringify( snapshot.val() ));
+    });
+  }
+
+  onPressLearnMore() {
+    databaseRef.push({
+      title: "aaa",
+      content: "bbb",
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        />
+        <Button
+          onPress={this.onPressLearnMore}
+          title="Learn More"
+          color="#841584"
+        />
       </View>
     );
   }
